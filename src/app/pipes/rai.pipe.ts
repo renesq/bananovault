5267295ca732c289bdf0967830c786d1643dc23a
renesq <35303112+renesq@users.noreply.github.com>
2018-02-27 15:53:52 +0100

    Update rai.pipe.ts

--- src/app/pipes/rai.pipe.ts
+++ src/app/pipes/rai.pipe.ts
@@ -7,34 +7,34 @@ import {AppSettingsService} from "../services/app-settings.service";
 export class RaiPipe implements PipeTransform {
   precision = 6;
 
-  mrai = 1000000000000000000000000000000;
-  krai = 1000000000000000000000000000;
+  ban = 100000000000000000000000000000;
+  mban = 100000000000000000000000000;
   rai  = 1000000000000000000000000;
 
   transform(value: any, args?: any): any {
     const opts = args.split(',');
-    let denomination = opts[0] || 'mrai';
+    let denomination = opts[0] || 'ban';
     const hideText = opts[1] || false;
 
     switch (denomination.toLowerCase()) {
       default:
-      case 'xrb': return `${(value / this.mrai).toFixed(6)}${!hideText ? ' NANO': ''}`;
-      case 'mnano':
-        const newVal = value / this.mrai < 0.000001 ? 0 : value / this.mrai; // New more precise toFixed function, but bugs on huge raw numbers
-        return `${this.toFixed(newVal, this.precision)}${!hideText ? ' NANO': ''}`;
-      case 'knano': return `${(value / this.krai).toFixed(3)}${!hideText ? ' knano': ''}`;
-      case 'nano': return `${(value / this.rai).toFixed(0)}${!hideText ? ' nano': ''}`;
+      case 'xrb': return `${(value / this.ban).toFixed(6)}${!hideText ? ' BAN': ''}`;  //this doesn't make much sense to me in the original nanovault because it's the same unit as the next one
+      case 'ban':
+        const newVal = value / this.ban < 0.000001 ? 0 : value / this.ban; // New more precise toFixed function, but bugs on huge raw numbers
+        return `${this.toFixed(newVal, this.precision)}${!hideText ? ' BAN': ''}`;
+      case 'mban': return `${(value / this.mban).toFixed(3)}${!hideText ? ' mBAN': ''}`;
+      case 'uban': return `${(value / this.uban).toFixed(0)}${!hideText ? ' uBAN': ''}`;
       case 'raw': return `${value}${!hideText ? ' raw': ''}`;
       case 'dynamic':
-        const rai = (value / this.rai);
+        const rai = (value / this.uban);
         if (rai >= 1000000) {
-          return `${(value / this.mrai).toFixed(this.precision)}${!hideText ? ' mRai': ''}`;
+          return `${(value / this.ban).toFixed(this.precision)}${!hideText ? ' BAN': ''}`;
         } else if (rai >= 1000) {
-          return `${(value / this.krai).toFixed(this.precision)}${!hideText ? ' kRai': ''}`;
+          return `${(value / this.mban).toFixed(this.precision)}${!hideText ? ' mBAN': ''}`;
         } else if (rai >= 0.00001) {
-          return `${(value / this.rai).toFixed(this.precision)}${!hideText ? ' Rai': ''}`;
+          return `${(value / this.uban).toFixed(this.precision)}${!hideText ? ' uBAN': ''}`;
         } else if (rai === 0) {
-          return `${value}${!hideText ? ' mRai': ''}`;
+          return `${value}${!hideText ? ' BAN': ''}`;
         } else {
           return `${value}${!hideText ? ' raw': ''}`;
         }
